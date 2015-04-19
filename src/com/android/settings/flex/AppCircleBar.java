33851fb73de53fb19ed03ba8636d8ffd5364d53d
Arnav Gosain <gosain.arnav@gmail.com>
2015-04-19 12:28:03 -0400

    Fix ups
    Change-Id: I066aa88cab161ebd61180d98154a30d051b5070f

--- src/com/android/settings/flex/AppCircleBar.java
+++ src/com/android/settings/flex/AppCircleBar.java
@@ -48,7 +48,7 @@ import java.util.ArrayList;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.temasek.SeekBarPreference;
+import com.android.settings.flex.SeekBarPreference;
 import com.android.settings.Utils;
 import com.android.settings.util.CMDProcessor;
 import com.android.settings.util.Helpers;
