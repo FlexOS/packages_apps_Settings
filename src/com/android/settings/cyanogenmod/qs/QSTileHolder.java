e3f8840e0fb30aff311dd06f6a643bc25ec8cd0d
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 01:38:22 +0300

    QS: Add heads up tile (2/2)
    Change-Id: I27eedf95ed200ab24f79380d6dd29c676775806b
    
    Conflicts:
    
    	res/values/cm_strings.xml
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/values/flex_strings.xml
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -157,6 +157,9 @@ public class QSTileHolder {
                 resourceName = "ic_qs_sync_on";
                 stringId = R.string.qs_sync_tile;
                 break;
+            case QSConstants.TILE_HEADS_UP:
+                resourceName = "ic_qs_heads_up_on";
+                stringId = R.string.qs_tile_heads_up;
             default:
                 return null;
         }
