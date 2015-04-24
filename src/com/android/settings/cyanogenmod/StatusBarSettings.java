52331234cde5f1e66d0dfe13c3d0a88baa5ed96c
Aniruddh Chandratre <chandratre.aniruddh@gmail.com>
2015-04-24 15:17:11 -0400

    Revert "Build fixes"
    This reverts commit ed74b1f9231f1339e49abbc9a86121239ca6cbbc.

--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -265,6 +265,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
             mStatusBarBatteryShowPercent.setSummary(
                     mStatusBarBatteryShowPercent.getEntries()[index]);
             return true;
+        }
         } else if (preference == mFlexLogoColor) {
             String hex = ColorPickerPreference.convertToARGB(
                     Integer.valueOf(String.valueOf(newValue)));
